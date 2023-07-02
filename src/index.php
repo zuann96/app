82e6d6909a4f375f5a4464c2b6dda81bb2dc4a94
zuann96 <joancat96@hotmail.com>
2023-07-02 21:42:45 +0200

    Reader and updater

--- src/index.php
+++ src/index.php
@@ -1,13 +1,33 @@
 <?php
 
-$con = new mysqli('mysql_db', 'root', 'root', 'mysql');
+
 
 echo "<pre>";
-var_dump(getenv(null));
+
+$host = getenv("MYSQL_HOST");
+$username = getenv("MYSQL_USER");
+$passwd = getenv("MYSQL_ROOT_PASSWORD");
+$dbname = getenv("MYSQL_DATABASE");
+
+
+
+$con = new mysqli($dbname, $username, $passwd);
+
+// mysqli::__construct(
+//     string $host = ini_get("mysqli.default_host"),
+//     string $username = ini_get("mysqli.default_user"),
+//     string $passwd = ini_get("mysqli.default_pw"),
+//     string $dbname = "",
+//     int $port = ini_get("mysqli.default_port"),
+//     string $socket = ini_get("mysqli.default_socket")
+// )
+
+    var_dump($host, $username, $passwd, $dbname );
 
 if($con)
-{
-    echo "Connection OK";
+{   
+    var_dump(mysqli_ping($con));
+    var_dump($host, $username, $passwd, $dbname );
 }
  
 ?>
